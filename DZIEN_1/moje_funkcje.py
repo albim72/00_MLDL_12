f824520d34a2ac8b0e44d8e1f208a29958b2844f
albim72 <marcin.albiniak72@gmail.com>
2023-12-18 10:28:22 +0100

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -10,3 +10,18 @@ print(policz(6,8.8,0.22,True))
 print(n)
 print(policz(4,6,2))
 
+
+#przykład 2
+
+print((lambda a:a+16)(4))
+s = lambda  a,b,z:(a*b-2*z)/(a+b+z)
+
+print(s(3,5,7))
+
+num = [56,7,8,-9,0,.7,13,65,12,99,-100,3,4,12,-34]
+
+nbparz = list(filter(lambda x:x%2==0,num))
+print(nbparz)
+
+cube = list(map(lambda x:x**3,num))
+print(cube)
